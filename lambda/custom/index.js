b17f04a952e1747dd4f6e88cd97cc22da3e9f4aa
rob <rob@robs-MacBook-Pro.local>
2017-12-08 20:11:03 +0000

    Updated index.js with correct handler to comply with lambda

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -4,18 +4,18 @@ var Alexa = require("alexa-sdk");
 var languageStrings = {
     'en': {
         'translation': {
-            'ABOUT_MESSAGE': 'This skill was written by Robert Balmbra',
-            'APP_NAME': '',
-            'COMMAND_MESSAGE': 'Say such commands like: \'\',\'\' and \'\'. So how can I help?',
+            'ABOUT_MESSAGE': 'This skill was written by ${FULL_NAME}',
+            'APP_NAME': '${APP_NAME}',
+            'COMMAND_MESSAGE': 'Say such commands like: \'${PHRASE_1}\',\'${PHRASE_2}\' and \'${PHRASE_3}\'. So how can I help?',
             'ASK_MESSAGE': 'So, how can I help?',
-            'LAUNCH_MESSAGE': 'Welcome to testdelete, so how can I help?',
+            'LAUNCH_MESSAGE': 'Welcome to ${APP_NAME}, so how can I help?',
             'CONN_ERROR_MESSAGE': 'Error - Failed to connect to server.',
             'GOODBYE': 'Goodbye!'
         }
     },
     'de-DE': {
         'translation': {
-            'ABOUT_MESSAGE': 'Diese Fähigkeit wurde von Robert Balmbra geschrieben',
+            'ABOUT_MESSAGE': 'Diese Fähigkeit wurde von ${FULL_NAME} geschrieben',
             'APP_NAME': '',
             'COMMAND_MESSAGE': '',
             'ASK_MESSAGE': 'Also, wie kann ich helfen?',
@@ -64,14 +64,14 @@ var handlers = {
         self.emit(":tellWithCard", speechOutput, this.t('APP_NAME'), speechOutput);
     }
 };
-var Handler = /** @class */ (function () {
-    function Handler(event, context, callback) {
+var handler = /** @class */ (function () {
+    function handler(event, context, callback) {
         var alexa = Alexa.handler(event, context);
         alexa.appId = "";
         alexa.resources = languageStrings;
         alexa.registerHandlers(handlers);
         alexa.execute();
     }
-    return Handler;
+    return handler;
 }());
-exports.Handler = Handler;
+exports.handler = handler;
